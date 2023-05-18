7f152c11638f4cb1e0a43ae9789d4280498798f6
Gustavo gomes <coven688@exemplo.com>
2023-05-18 17:58:23 -0300

    Criando o hero e importanto a fonte Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp') // Aqui importamos o gulp
 const sass = require('gulp-sass')(require('sass')); //aqui importamos o sass
+const imagemin = require('gulp-imagemin')
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -7,7 +8,13 @@ function styles() {
         .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel(styles, images)
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
